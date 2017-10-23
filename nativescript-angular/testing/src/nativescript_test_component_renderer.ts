8ea09c88f598a38b8e08e9d4dff5390402ee3ac1
Justin DuJardin <justin@dujardinconsulting.com>
2017-10-23 16:43:03 -0700

    chore: so it is pretty straight forward after all
     - nice job Angular and {N} teams.

--- nativescript-angular/testing/src/nativescript_test_component_renderer.ts
+++ nativescript-angular/testing/src/nativescript_test_component_renderer.ts
@@ -1,23 +1,31 @@
-import {Inject, Injectable} from '@angular/core';
+import {Injectable} from '@angular/core';
 import {TestComponentRenderer} from '@angular/core/testing';
-import {DOCUMENT, ɵgetDOM as getDOM} from '@angular/platform-browser';
+import {topmost} from 'tns-core-modules/ui/frame';
+import {LayoutBase} from 'tns-core-modules/ui/layouts/layout-base';
+import {AbsoluteLayout} from 'tns-core-modules/ui/layouts/absolute-layout';
+import {PercentLength} from 'tns-core-modules/ui/styling/style-properties';
 
 /**
  * A NativeScript based implementation of the TestComponentRenderer.
  */
 @Injectable()
 export class NativeScriptTestComponentRenderer extends TestComponentRenderer {
-  constructor(@Inject(DOCUMENT) private _doc: any /** TODO #9100 */) { super(); }
+  constructor() {
+    super();
+  }
 
   insertRootElement(rootElId: string) {
-    const rootEl = <HTMLElement>getDOM().firstChild(
-        getDOM().content(getDOM().createTemplate(`<div id="${rootElId}"></div>`)));
+    const page = topmost().currentPage;
 
-    // TODO(juliemr): can/should this be optional?
-    const oldRoots = getDOM().querySelectorAll(this._doc, '[id^=root]');
-    for (let i = 0; i < oldRoots.length; i++) {
-      getDOM().remove(oldRoots[i]);
-    }
-    getDOM().appendChild(this._doc.body, rootEl);
+    const layout = new AbsoluteLayout();
+    layout.id = rootElId;
+    layout.width = layout.height = PercentLength.parse('100%');
+    AbsoluteLayout.setLeft(layout, 0);
+    AbsoluteLayout.setTop(layout, 0);
+
+    const rootLayout = page.layoutView as LayoutBase;
+    rootLayout.addChild(layout);
   }
+
 }
+
--- nativescript-angular/testing/src/testing.ts
+++ nativescript-angular/testing/src/testing.ts
@@ -1,14 +1,26 @@
 import {NgModule} from '@angular/core';
 import {TestComponentRenderer} from '@angular/core/testing';
 import {NativeScriptTestComponentRenderer} from './nativescript_test_component_renderer';
+import {COMMON_PROVIDERS} from '../../platform-common';
+import {APP_ROOT_VIEW} from '../../platform-providers';
+import {topmost} from 'tns-core-modules/ui/frame';
+import {View} from 'tns-core-modules/ui/core/view';
 
 /**
- * NgModule for testing.
- *
- * @stable
+ * Get a reference to the root application view.
+ */
+export function testingRootView(): View {
+  return topmost().currentPage.layoutView;
+}
+
+/**
+ * NativeScript testing support module. Enables use of TestBed for angular components, directives,
+ * pipes, and services.
  */
 @NgModule({
   providers: [
+    COMMON_PROVIDERS,
+    {provide: APP_ROOT_VIEW, useFactory: testingRootView},
     {provide: TestComponentRenderer, useClass: NativeScriptTestComponentRenderer},
   ]
 })
